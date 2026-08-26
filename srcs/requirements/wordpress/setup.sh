aacfeb1421bbe0abb8f44d5af9d010415e9d8763
Yeray-Dev <133887057+Yeray-Dev@users.noreply.github.com>
2026-08-26 15:55:46 +0200

    fix(compose): name images after their services

--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -22,6 +22,9 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 fi
 if ! wp core is-installed --path=/var/www/html --allow-root; then
   wp core install --path=/var/www/html --url=https://$DOMAIN_NAME --title=Inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+
+  wp user create $WP_USER $WP_USER_EMAIL --role=author \
+    --user_pass=$WP_USER_PASSWORD --path=/var/www/html --allow-root
 fi
 
 chown -R www-data:www-data /var/www/html
