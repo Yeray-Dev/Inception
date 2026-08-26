6b21216ed3e4f980ae6e78e8efdbf1091c53018c
Yeray-Dev <133887057+Yeray-Dev@users.noreply.github.com>
2026-08-26 15:27:16 +0200

    fix: MariaDB init.sh

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-#DECLARAMOS VARIABLES DESDE SECRETS
+# DECLARAMOS VARIABLES DESDE SECRETS
 DB_PASSWORD=$(cat /run/secrets/db_password)
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
-#SI LA BBDD NO EXISTE LA CREAMOS
-if [ ! -d /var/lib/mysql/mysql ]; then
-  mysql_install_db --user=mysql --datadir=/var/lib/mysql
-fi
-
 mkdir -p /run/mysqld
 chown mysql:mysql /run/mysqld
+
+# SI LA BBDD NO EXISTE LA CREAMOS
+if [ ! -d /var/lib/mysql/mysql ]; then
+  mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
 # LANZAMOS EL PROCESO EN SEGUNDO PLANO
 mariadbd --user=mysql &
+MARIADB_PID=$!
 
 # INTENTAMOS CONECTAR CON MARIADB, USAMOS UN CONTADOR, SI EN 30 INTENTOS NO SE CONSIGUE CONTACTAR TERMINAMOS EL PROCESO
 RETRIES=30
@@ -27,15 +28,17 @@ if [ $RETRIES -eq 0 ]; then
 fi
 
 # CON LA BBDD LEVANTADA CREAMOS LAS TABLAS Y USUARIOS SINO EXISTIERAN
-mysql -u root <<EOF
-  CREATE DATABASE IF NOT EXISTS $DB_NAME;
-  CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-  GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-  FLUSH PRIVILEGES;
-  ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+  mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 EOF
 
+  mysqladmin -u root -p"$DB_ROOT_PASSWORD" shutdown
+  wait "$MARIADB_PID"
+fi
+
 # CUANDO EL SCRIPT ACABE TIENE QUE PASAR MARIADB AL PID 1
-# PARA ELLO PRIMERO TERMINAR CON EL PROCESO QUE OCUPA EL PUETO 3306 Y LUEGO LO LEVANTA COMO PID1
-mysqladmin -u root -p"$DB_ROOT_PASSWORD" shutdown
-exec mariadbd --user=mysql
+exec mariadbd --user=mysql --bind-address=0.0.0.0
